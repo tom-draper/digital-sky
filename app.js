e0ad9b916ffcd730561c90558a3f7db83e16a910
Tom Draper <tomjdraper1@gmail.com>
2022-03-16 11:58:24 +0000

    sunset layers

--- app.js
+++ app.js
@@ -17,8 +17,9 @@ function TupleSet() {
   };
 
   this.delete = function ([first, second]) {
-    if (!this.data.has(first) || !this.data.get(first).has(second))
+    if (!this.data.has(first) || !this.data.get(first).has(second)) {
       return false;
+    }
 
     this.data.get(first).delete(second);
     if (this.data.get(first).size === 0) {
@@ -36,6 +37,10 @@ function createGrid(h, w) {
 
 function formatColour(r, g, b, a) {
   return `rgba(${r},${g},${b},${a})`;
+}
+
+function formatColour2(colour) {
+  return `rgba(${colour[0]},${colour[1]},${colour[2]},${colour[3]})`;
 }
 
 function randInt(min, max) {
@@ -64,8 +69,8 @@ function initSky(pixelSize) {
 }
 
 function mutateColour(colour, step) {
-  let mutatedColour = [...colour];
-  switch (randInt(0, 60)) {
+  let mutatedColour = Object.values(colour);
+  switch (randInt(0, 50)) {
     case 0:
       mutatedColour[0] = Math.min(colour[0] + step, 255);
       break;
@@ -117,58 +122,57 @@ function mutateColourInPlace(colour, step) {
 
 function colourSpread(x, y, colour, seen, toPaint) {
   let nextColour = mutateColour(colour, 1);
+
   if (y < h - 1 && !seen.has([x, y + 1])) {
-    seen.add([x, y + 1]);
     toPaint.push([x, y + 1, nextColour]);
+    seen.add([x, y + 1]);
   }
   if (x < w - 1 && !seen.has([x + 1, y])) {
-    seen.add([x + 1, y]);
     toPaint.push([x + 1, y, nextColour]);
+    seen.add([x + 1, y]);
   }
   if (y > 0 && !seen.has([x, y - 1])) {
-    seen.add([x, y - 1]);
     toPaint.push([x, y - 1, nextColour]);
+    seen.add([x, y - 1]);
   }
   if (x > 0 && !seen.has([x - 1, y])) {
-    seen.add([x - 1, y]);
     toPaint.push([x - 1, y, nextColour]);
+    seen.add([x - 1, y]);
   }
   if (x > 0 && y > 0 && !seen.has([x - 1, y - 1])) {
-    seen.add([x - 1, y - 1]);
     toPaint.push([x - 1, y - 1, nextColour]);
+    seen.add([x - 1, y - 1]);
   }
   if (x > 0 && y < h - 1 && !seen.has([x - 1, y + 1])) {
-    seen.add([x - 1, y + 1]);
     toPaint.push([x - 1, y + 1, nextColour]);
+    seen.add([x - 1, y + 1]);
   }
   if (x < w - 1 && y > 0 && !seen.has([x + 1, y - 1])) {
-    seen.add([x + 1, y - 1]);
     toPaint.push([x + 1, y - 1, nextColour]);
+    seen.add([x + 1, y - 1]);
   }
   if (x < w - 1 && y < h - 1 && !seen.has([x + 1, y + 1])) {
-    seen.add([x + 1, y + 1]);
     toPaint.push([x + 1, y + 1, nextColour]);
+    seen.add([x + 1, y + 1]);
   }
 }
 
-function colourSky(grid, startColour) {
+function colourSky(grid, skyConfig) {
   console.log("Colouring sky...");
 
-  let startX = randInt(0, w - 1);
-  let startY = randInt(0, h - 1);
-  // let startColour = [255, 192, 203];  // Pink
-  // let startColour = [135, 206, 235, 1]; // Blue
+  let start = [randInt(0, w - 1), randInt(0, h - 1)];
+  let startColour = [...skyConfig.colour, skyConfig.opacity];
 
   let seen = new TupleSet();
   let toPaint = [];
 
-  toPaint.push([startX, startY, startColour]);
-  seen.add([startX, startY]);
+  toPaint.push([start[0], start[1], startColour]);
+  seen.add(start);
 
   let x, y, colour;
   while (toPaint.length > 0) {
     [x, y, colour] = nextPixel(toPaint);
-    grid[y][x].style.background = formatColour(...colour);
+    grid[y][x].style.background = formatColour2(colour);
     colourSpread(x, y, colour, seen, toPaint);
   }
   console.log("Complete");
@@ -253,9 +257,7 @@ function createCloudBase(grid, cloud, startColour, sizeRange, pH, pV) {
   seen.add(start);
 
   cloud.start = start;
-  cloud.level = [
-    [start]
-  ];
+  cloud.level = [[start]];
 
   let cloudSize = 1;
   let x, y, colour;
@@ -315,35 +317,33 @@ function createCloud(grid, layers) {
   let cloud = {};
 
   createCloudBase(
-    grid, 
-    cloud, 
-    [...layers[0].colour, layers[0].opacity], 
-    [layers[0].minSize, layers[0].maxSize], 
-    layers[0].pH, 
+    grid,
+    cloud,
+    [...layers[0].colour, layers[0].opacity],
+    [layers[0].minSize, layers[0].maxSize],
+    layers[0].pH,
     layers[0].pV
   );
-  
+
   for (let i = 1; i < layers.length; i++) {
     addCloudLayer(
-      grid, 
-      cloud, 
-      [...layers[i].colour, layers[i].opacity], 
-      [layers[i].minSize, layers[i].maxSize], 
-      layers[i].pH, 
-      layers[i].pV);
+      grid,
+      cloud,
+      [...layers[i].colour, layers[i].opacity],
+      [layers[i].minSize, layers[i].maxSize],
+      layers[i].pH,
+      layers[i].pV
+    );
   }
 
   return cloud;
 }
 
 function createClouds(grid, cloudConfig) {
-  console.log("Creating clouds...");
-
   let clouds = [];
   for (let i = 0; i < cloudConfig.quantity; i++) {
     clouds[i] = createCloud(grid, cloudConfig.layers);
   }
-  console.log("Complete");
   return clouds;
 }
 
@@ -355,7 +355,7 @@ function createStarPixel(colour) {
 }
 
 function onSky(x, y) {
-  return (x >= 0) && (y >= 0) && (x < w) && (y < h);
+  return x >= 0 && y >= 0 && x < w && y < h;
 }
 
 function starColour() {
@@ -369,7 +369,12 @@ function createStar(x, y, grid) {
   grid[y][x].appendChild(starPixel);
 
   let p = 0.1;
-  [[x+1, y], [x, y+1], [x-1, y], [x, y-1]].forEach(coord => {
+  [
+    [x + 1, y],
+    [x, y + 1],
+    [x - 1, y],
+    [x, y - 1],
+  ].forEach((coord) => {
     if (Math.random() < p && onSky(...coord)) {
       starPixel = createStarPixel(colour);
       grid[coord[1]][coord[0]].appendChild(starPixel);
@@ -380,15 +385,13 @@ function createStar(x, y, grid) {
   return star;
 }
 
-function createStars(grid, density) {
-  console.log("Creating stars...");
+function createStars(grid, starsConfig) {
   let stars = [];
-  let n = h * w * density;
+  let n = h * w * starsConfig.density;
   for (let i = 0; i < n; i++) {
     let star = createStar(randInt(0, w - 1), randInt(0, h - 1), grid);
     stars.push(star);
   }
-  console.log("Complete");
   return stars;
 }
 
@@ -400,93 +403,126 @@ function createSunsetPixel(colour) {
 }
 
 function getRGBValues(str) {
-  var vals = str.substring(str.indexOf('(') +1, str.length -1).split(', ');
+  var vals = str.substring(str.indexOf("(") + 1, str.length - 1).split(", ");
   return [parseInt(vals[0]), parseInt(vals[1]), parseInt(vals[2])];
 }
 
-function sunsetSpread(x, y, colour, toPaint, seen) {
-  let nextColour = mutateColour(colour, 1);
+function sunsetSpread(x, y, colour, toPaint, seen, colourStep) {
+  let nextColour = mutateColour(colour, colourStep);
 
   if (y < h - 1 && !seen.has([x, y + 1])) {
     seen.add([x, y + 1]);
     toPaint.push([x, y + 1, nextColour]);
   }
-
   if (y > 0 && !seen.has([x, y - 1])) {
     seen.add([x, y - 1]);
     toPaint.push([x, y - 1, nextColour]);
   }
-
   if (x < w - 1 && !seen.has([x + 1, y])) {
     seen.add([x + 1, y]);
     toPaint.push([x + 1, y, nextColour]);
   }
-
   if (x > 0 && !seen.has([x - 1, y])) {
     seen.add([x - 1, y]);
     toPaint.push([x - 1, y, nextColour]);
   }
 }
 
-function warpedDistance(x1, y1, x2, y2) {
-  let x = y2 - y1;
-  let y = (x2 - x1) * 0.3;
+function warpedDistance(x1, y1, x2, y2, xStretch, yStretch) {
+  let x = y2 - y1 * yStretch;
+  let y = (x2 - x1) * xStretch;
   return Math.sqrt(x * x + y * y);
 }
 
-function createSunset(grid) {
-  console.log("Creating sunset...");
-  let maxD = h * 0.7;
+function combineColours(colour1, colour2) {
+  let a1 = colour1[3];
+  let a2 = colour2[3];
+  let a = a1 + a2 * (1 - a1);
+  let colour = [
+    (colour1[0] * a1 + colour2[0] * a2 * (1 - a1)) / a,
+    (colour1[1] * a1 + colour2[1] * a2 * (1 - a1)) / a,
+    (colour1[2] * a1 + colour2[2] * a2 * (1 - a1)) / a,
+    a
+  ];
+  return colour;
+}
+
+function applyColour(x, y, grid, colour) {
+  let originalColour = [...getRGBValues(grid[y][x].style.background), 1];
+  let combinedColour = combineColours(colour, originalColour);
+  grid[y][x].style.background = formatColour2(combinedColour);
+}
+
+function createSunsetLayer(grid, layer) {
+  let maxD = h * layer.proportion;
 
   let seen = new TupleSet();
   let toPaint = [];
 
-  let maxOpacity = 0.45;
-  let colour = [253, 94, 83, maxOpacity];
-  
+  let colour = [253, 94, 83, layer.maxOpacity];
+
   let start = [randInt(0, w), h - 1];
-  toPaint.push([start[0], start[1], colour]);
+  toPaint.push([start[0], start[1], layer.colour]);
   seen.add(start);
-  
+
   let scale;
   while (toPaint.length > 0) {
     [x, y, colour] = nextPixel(toPaint);
-    scale = 1 - (warpedDistance(x, y, start[0], start[1])/maxD);
+    scale =
+      1 -
+      warpedDistance(
+        x,
+        y,
+        start[0],
+        start[1],
+        layer.xStretch,
+        layer.yStretch
+      ) /
+        maxD;
     if (scale > 0) {
-      colour[3] = maxOpacity * scale;
-      grid[y][x].appendChild(createSunsetPixel(colour));
-      sunsetSpread(x, y, colour, toPaint, seen);
+      colour[3] = layer.maxOpacity * scale;
+      applyColour(x, y, grid, colour);
+      sunsetSpread(x, y, colour, toPaint, seen, layer.colourMutationSpeed);
     }
   }
+}
 
-  console.log("Complete");
+function createSunset(grid, sunsetConfig) {
+  for (let i = 0; i < sunsetConfig.layers.length; i++) {
+    createSunsetLayer(grid, sunsetConfig.layers[i]);
+  }
 }
 
 function createSky(config) {
   let grid = initSky(config.sky.pixelSize);
-  
-  colourSky(grid, [...config.sky.colour, config.sky.opacity]);
 
-  if (config.sunset) {
-    createSunset(grid);
+  colourSky(grid, config.sky);
+
+  if (config.sunset.include) {
+    console.log("Creating sunset...");
+    createSunset(grid, config.sunset);
+    console.log("Complete");
   }
-  
+
   if (config.stars.include) {
-    createStars(grid, config.stars.density);
+    console.log("Creating stars...");
+    createStars(grid, config.stars);
+    console.log("Complete");
   }
-  
-  if (config.clouds.include) {
-    createClouds(grid, config.clouds);
-  }
-}
 
+  if (config.clouds.include) {
+    console.log("Creating clouds...");
+    createClouds(grid, config.clouds);
+    console.log("Complete");
+  }
+  console.log("Rendering sky...");
+}
 
 let skyColours = {
   daytime: [135, 206, 235],
   evening: [255, 192, 203],
-  nighttime: [19, 19, 19]
+  nighttime: [19, 19, 19],
 };
-
 
 let config = presetSunset;
 
--- presets.js
+++ presets.js
@@ -3,315 +3,307 @@
 let defaultHeight = 720;
 let defaultWidth = 1280;
 
-// Light day
-let presetLightDay = {
-  animate: false,
-  sky: {
+function defaultSky(colour) {
+  return {
     height: defaultHeight,
     width: defaultWidth,
     pixelSize: 1,
-    colour: [135, 206, 235],
-    opacity: 1,
-  },
-  stars: {
-    include: false,
-    density: 0,
-  },
+    colour: colour,
+    opacity: 1
+  };
+} 
+
+let stars1 = {
+  include: true,
+  density: 0.005,
+};
+let starsDense = {
+  include: true,
+  density: 0.01,
+};
+let starsNone = {
+  include: false,
+  density: null,
+};
+
+let sunset1 = {
+  include: true,
+  layers: [
+    {
+      colour: [253, 94, 83],
+      maxOpacity: 0.45,
+      // The proportion of the height used as the max distance that the sunset travels
+      proportion: 0.7,
+      // Magnitude of the inc/dec of one rgb value with each colour step
+      colourMutationSpeed: 1,
+      xStretch: 0.5,  // >1 (thinner), <1 (wider) 
+      yStretch: 1,  // >1 (shorter), <1 (taller) 
+    },
+    {
+      colour: [255, 201, 34],
+      maxOpacity: 0.4,
+      proportion: 0.7,
+      colourMutationSpeed: 1,
+      xStretch: 0.3,
+      yStretch: 1,
+    }
+  ]
+};
+let sunsetNone = {
+  include: false,
+  colour: null,
+  maxOpacity: null,
+  xStretch: null,
+  yStretch: null,
+};
+
+let clouds1 = {
+  include: true,
+  quantity: 10,
+  // Cloud layers are created in order
+  layers: [
+    {
+      colour: [235, 235, 235],
+      opacity: 0.2,
+      minSize: 100,
+      maxSize: 10000,
+      pH: 0.7, // Probability of horizontal expansion
+      pV: 0.3, // Probability of vertical expansion
+    },
+    {
+      colour: [235, 235, 235],
+      opacity: 0.15,
+      minSize: 100,
+      maxSize: 10000,
+      pH: 0.7,
+      pV: 0.3,
+    },
+    {
+      colour: [235, 235, 235],
+      opacity: 0.15,
+      minSize: 100,
+      maxSize: 10000,
+      pH: 0.7,
+      pV: 0.3,
+    },
+    {
+      colour: [235, 235, 235],
+      opacity: 0.15,
+      minSize: 100,
+      maxSize: 10000,
+      pH: 0.7,
+      pV: 0.3,
+    },
+    {
+      colour: [240, 211, 201],
+      opacity: 0.3,
+      minSize: 100,
+      maxSize: 2000,
+      pH: 0.6,
+      pV: 0.2,
+    },
+    {
+      colour: [240, 211, 201],
+      opacity: 0.15,
+      minSize: 100,
+      maxSize: 2000,
+      pH: 0.6,
+      pV: 0.2,
+    },
+    {
+      colour: [173, 216, 230],
+      opacity: 0.15,
+      minSize: 100,
+      maxSize: 1000,
+      pH: 0.6,
+      pV: 0.2,
+    },
+    {
+      colour: [173, 216, 230],
+      opacity: 0.15,
+      minSize: 100,
+      maxSize: 1000,
+      pH: 0.6,
+      pV: 0.2,
+    },
+    {
+      colour: [240, 240, 240],
+      opacity: 0.2,
+      minSize: 100,
+      maxSize: 1000,
+      pH: 0.6,
+      pV: 0.2,
+    },
+  ],
+};
+let cloudsCloudy = {
+  include: true,
+  quantity: 30,
+  // Cloud layers are created in order
+  layers: [
+    {
+      colour: [183, 183, 178],
+      opacity: 0.8,
+      minSize: 100,
+      maxSize: 50000,
+      pH: 0.8, // Probability of horizontal expansion
+      pV: 0.5, // Probability of vertical expansion
+    },
+    {
+      colour: [183, 183, 178],
+      opacity: 0.7,
+      minSize: 100,
+      maxSize: 50000,
+      pH: 0.8,
+      pV: 0.5,
+    },
+    {
+      colour: [183, 183, 178],
+      opacity: 0.15,
+      minSize: 100,
+      maxSize: 10000,
+      pH: 0.7,
+      pV: 0.3,
+    },
+    {
+      colour: [183, 183, 178],
+      opacity: 0.3,
+      minSize: 100,
+      maxSize: 10000,
+      pH: 0.7,
+      pV: 0.3,
+    },
+    {
+      colour: [183, 183, 178],
+      opacity: 0.3,
+      minSize: 100,
+      maxSize: 10000,
+      pH: 0.7,
+      pV: 0.3,
+    },
+    {
+      colour: [183, 183, 178],
+      opacity: 0.3,
+      minSize: 100,
+      maxSize: 10000,
+      pH: 0.7,
+      pV: 0.3,
+    },
+  ],
+};
+let cloudsMild = {
+  include: true,
+  quantity: 6,
+  // Cloud layers are created in order
+  layers: [
+    {
+      colour: [200, 200, 200],
+      opacity: 0.2,
+      minSize: 100,
+      maxSize: 30000,
+      pH: 0.7, // Probability of horizontal expansion
+      pV: 0.3, // Probability of vertical expansion
+    },
+    {
+      colour: [235, 235, 235],
+      opacity: 0.15,
+      minSize: 100,
+      maxSize: 30000,
+      pH: 0.7,
+      pV: 0.3,
+    },
+    {
+      colour: [235, 235, 235],
+      opacity: 0.15,
+      minSize: 100,
+      maxSize: 30000,
+      pH: 0.7,
+      pV: 0.3,
+    },
+    {
+      colour: [235, 235, 235],
+      opacity: 0.15,
+      minSize: 100,
+      maxSize: 10000,
+      pH: 0.7,
+      pV: 0.3,
+    },
+    {
+      colour: [240, 211, 201],
+      opacity: 0.3,
+      minSize: 100,
+      maxSize: 10000,
+      pH: 0.6,
+      pV: 0.2,
+    },
+    {
+      colour: [240, 211, 201],
+      opacity: 0.15,
+      minSize: 100,
+      maxSize: 10000,
+      pH: 0.6,
+      pV: 0.2,
+    },
+    {
+      colour: [173, 216, 230],
+      opacity: 0.1,
+      minSize: 100,
+      maxSize: 10000,
+      pH: 0.6,
+      pV: 0.2,
+    },
+    {
+      colour: [173, 216, 230],
+      opacity: 0.1,
+      minSize: 100,
+      maxSize: 10000,
+      pH: 0.6,
+      pV: 0.2,
+    }
+  ],
+};
+
+// Light day
+let presetLightDay = {
+  animate: false,
+  windspeed: 0.2,
+  sky: defaultSky([135, 206, 235]),
+  stars: starsNone,
   sun: false,
   moon: false,
-  sunset: false,
-  clouds: {
-    include: true,
-    quantity: 10,
-    // Cloud layers are created in order
-    layers: [
-      {
-        colour: [235, 235, 235],
-        opacity: 0.2,
-        minSize: 100,
-        maxSize: 10000,
-        pH: 0.7, // Probability of horizontal expansion
-        pV: 0.3, // Probability of vertical expansion
-      },
-      {
-        colour: [235, 235, 235],
-        opacity: 0.15,
-        minSize: 100,
-        maxSize: 10000,
-        pH: 0.7,
-        pV: 0.3,
-      },
-      {
-        colour: [235, 235, 235],
-        opacity: 0.15,
-        minSize: 100,
-        maxSize: 10000,
-        pH: 0.7,
-        pV: 0.3,
-      },
-      {
-        colour: [235, 235, 235],
-        opacity: 0.15,
-        minSize: 100,
-        maxSize: 10000,
-        pH: 0.7,
-        pV: 0.3,
-      },
-      {
-        colour: [240, 211, 201],
-        opacity: 0.3,
-        minSize: 100,
-        maxSize: 2000,
-        pH: 0.6,
-        pV: 0.2,
-      },
-      {
-        colour: [240, 211, 201],
-        opacity: 0.15,
-        minSize: 100,
-        maxSize: 2000,
-        pH: 0.6,
-        pV: 0.2,
-      },
-      {
-        colour: [173, 216, 230],
-        opacity: 0.15,
-        minSize: 100,
-        maxSize: 1000,
-        pH: 0.6,
-        pV: 0.2,
-      },
-      {
-        colour: [173, 216, 230],
-        opacity: 0.15,
-        minSize: 100,
-        maxSize: 1000,
-        pH: 0.6,
-        pV: 0.2,
-      },
-      {
-        colour: [240, 240, 240],
-        opacity: 0.2,
-        minSize: 100,
-        maxSize: 1000,
-        pH: 0.6,
-        pV: 0.2,
-      },
-    ],
-  },
+  sunset: sunsetNone,
+  clouds: clouds1
 };
 
 
 let presetSunset = {
   animate: false,
-  sky: {
-    height: defaultHeight,
-    width: defaultWidth,
-    pixelSize: 1,
-    colour: [135, 206, 235],
-    opacity: 1,
-  },
-  stars: {
-    include: false,
-    density: 0,
-  },
+  windspeed: 0.2,
+  sky: defaultSky([135, 206, 235]),
+  stars: starsNone,
   sun: false,
   moon: false,
-  sunset: true,
-  clouds: {
-    include: true,
-    quantity: 10,
-    // Cloud layers are created in order
-    layers: [
-      {
-        colour: [235, 235, 235],
-        opacity: 0.2,
-        minSize: 100,
-        maxSize: 10000,
-        pH: 0.7, // Probability of horizontal expansion
-        pV: 0.3, // Probability of vertical expansion
-      },
-      {
-        colour: [235, 235, 235],
-        opacity: 0.15,
-        minSize: 100,
-        maxSize: 10000,
-        pH: 0.7,
-        pV: 0.3,
-      },
-      {
-        colour: [235, 235, 235],
-        opacity: 0.15,
-        minSize: 100,
-        maxSize: 10000,
-        pH: 0.7,
-        pV: 0.3,
-      },
-      {
-        colour: [235, 235, 235],
-        opacity: 0.15,
-        minSize: 100,
-        maxSize: 10000,
-        pH: 0.7,
-        pV: 0.3,
-      },
-      {
-        colour: [240, 211, 201],
-        opacity: 0.3,
-        minSize: 100,
-        maxSize: 2000,
-        pH: 0.6,
-        pV: 0.2,
-      },
-      {
-        colour: [240, 211, 201],
-        opacity: 0.15,
-        minSize: 100,
-        maxSize: 2000,
-        pH: 0.6,
-        pV: 0.2,
-      },
-      {
-        colour: [173, 216, 230],
-        opacity: 0.15,
-        minSize: 100,
-        maxSize: 1000,
-        pH: 0.6,
-        pV: 0.2,
-      },
-      {
-        colour: [173, 216, 230],
-        opacity: 0.15,
-        minSize: 100,
-        maxSize: 1000,
-        pH: 0.6,
-        pV: 0.2,
-      },
-      {
-        colour: [240, 240, 240],
-        opacity: 0.2,
-        minSize: 100,
-        maxSize: 1000,
-        pH: 0.6,
-        pV: 0.2,
-      },
-    ],
-  },
+  sunset: sunset1,
+  clouds: clouds1
 };
 
 let presetPinkSky = {
   animate: false,
-  sky: {
-    height: defaultHeight,
-    width: defaultWidth,
-    pixelSize: 1,
-    colour: [255, 192, 203],
-    opacity: 1,
-  },
-  stars: {
-    include: false,
-    density: 0,
-  },
+  sky: defaultSky([255, 192, 203]),
+  stars: starsNone,
   sun: false,
   moon: false,
-  sunset: false,
-  clouds: {
-    include: true,
-    quantity: 10,
-    // Cloud layers are created in order
-    layers: [
-      {
-        colour: [235, 235, 235],
-        opacity: 0.2,
-        minSize: 100,
-        maxSize: 10000,
-        pH: 0.7, // Probability of horizontal expansion
-        pV: 0.3, // Probability of vertical expansion
-      },
-      {
-        colour: [235, 235, 235],
-        opacity: 0.15,
-        minSize: 100,
-        maxSize: 10000,
-        pH: 0.7,
-        pV: 0.3,
-      },
-      {
-        colour: [235, 235, 235],
-        opacity: 0.15,
-        minSize: 100,
-        maxSize: 10000,
-        pH: 0.7,
-        pV: 0.3,
-      },
-      {
-        colour: [235, 235, 235],
-        opacity: 0.15,
-        minSize: 100,
-        maxSize: 10000,
-        pH: 0.7,
-        pV: 0.3,
-      },
-      {
-        colour: [240, 211, 201],
-        opacity: 0.3,
-        minSize: 100,
-        maxSize: 2000,
-        pH: 0.6,
-        pV: 0.2,
-      },
-      {
-        colour: [240, 211, 201],
-        opacity: 0.15,
-        minSize: 100,
-        maxSize: 2000,
-        pH: 0.6,
-        pV: 0.2,
-      },
-      {
-        colour: [173, 216, 230],
-        opacity: 0.15,
-        minSize: 100,
-        maxSize: 1000,
-        pH: 0.6,
-        pV: 0.2,
-      },
-      {
-        colour: [173, 216, 230],
-        opacity: 0.15,
-        minSize: 100,
-        maxSize: 1000,
-        pH: 0.6,
-        pV: 0.2,
-      },
-      {
-        colour: [240, 240, 240],
-        opacity: 0.2,
-        minSize: 100,
-        maxSize: 1000,
-        pH: 0.6,
-        pV: 0.2,
-      },
-    ],
-  },
+  sunset: sunsetNone,
+  clouds: clouds1
 };
 
 let presetLightDayCloudy = {
   animate: false,
-  sky: {
-    height: defaultHeight,
-    width: defaultWidth,
-    pixelSize: 1,
-    colour: [135, 206, 235],
-    opacity: 1,
-  },
-  stars: {
-    include: false,
-    density: 0,
-  },
+  sky: defaultSky([135, 206, 235]),
+  stars: starsNone,
   sun: false,
   moon: false,
-  sunset: false,
+  sunset: sunsetNone,
   clouds: {
     include: true,
     quantity: 10,
@@ -395,188 +387,22 @@ let presetLightDayCloudy = {
 
 let presetLightDayStarry = {
   animate: false,
-  sky: {
-    height: defaultHeight,
-    width: defaultWidth,
-    pixelSize: 1,
-    colour: [135, 206, 235],
-    opacity: 1,
-  },
-  stars: {
-    include: true,
-    density: 0.005,
-  },
+  sky: defaultSky([135, 206, 235]),
+  stars: stars1,
   sun: false,
   moon: false,
-  sunset: false,
-  clouds: {
-    include: true,
-    quantity: 10,
-    // Cloud layers are created in order
-    layers: [
-      {
-        colour: [235, 235, 235],
-        opacity: 0.2,
-        minSize: 100,
-        maxSize: 10000,
-        pH: 0.7, // Probability of horizontal expansion
-        pV: 0.3, // Probability of vertical expansion
-      },
-      {
-        colour: [235, 235, 235],
-        opacity: 0.15,
-        minSize: 100,
-        maxSize: 10000,
-        pH: 0.7,
-        pV: 0.3,
-      },
-      {
-        colour: [235, 235, 235],
-        opacity: 0.15,
-        minSize: 100,
-        maxSize: 10000,
-        pH: 0.7,
-        pV: 0.3,
-      },
-      {
-        colour: [235, 235, 235],
-        opacity: 0.15,
-        minSize: 100,
-        maxSize: 10000,
-        pH: 0.7,
-        pV: 0.3,
-      },
-      {
-        colour: [240, 211, 201],
-        opacity: 0.3,
-        minSize: 100,
-        maxSize: 2000,
-        pH: 0.6,
-        pV: 0.2,
-      },
-      {
-        colour: [240, 211, 201],
-        opacity: 0.15,
-        minSize: 100,
-        maxSize: 2000,
-        pH: 0.6,
-        pV: 0.2,
-      },
-      {
-        colour: [173, 216, 230],
-        opacity: 0.15,
-        minSize: 100,
-        maxSize: 1000,
-        pH: 0.6,
-        pV: 0.2,
-      },
-      {
-        colour: [173, 216, 230],
-        opacity: 0.15,
-        minSize: 100,
-        maxSize: 1000,
-        pH: 0.6,
-        pV: 0.2,
-      },
-      {
-        colour: [240, 240, 240],
-        opacity: 0.2,
-        minSize: 100,
-        maxSize: 1000,
-        pH: 0.6,
-        pV: 0.2,
-      },
-    ],
-  },
+  sunset: sunsetNone,
+  clouds: clouds1
 };
 
 let presetNight = {
   animate: false,
-  sky: {
-    height: defaultHeight,
-    width: defaultWidth,
-    pixelSize: 1,
-    colour: [40, 30, 45],
-    opacity: 1,
-  },
-  stars: {
-    include: true,
-    density: 0.006,
-  },
+  sky: defaultSky([40, 30, 45]),
+  stars: stars1,
   sun: false,
   moon: false,
-  sunset: false,
-  clouds: {
-    include: true,
-    quantity: 6,
-    // Cloud layers are created in order
-    layers: [
-      {
-        colour: [200, 200, 200],
-        opacity: 0.2,
-        minSize: 100,
-        maxSize: 30000,
-        pH: 0.7, // Probability of horizontal expansion
-        pV: 0.3, // Probability of vertical expansion
-      },
-      {
-        colour: [235, 235, 235],
-        opacity: 0.15,
-        minSize: 100,
-        maxSize: 30000,
-        pH: 0.7,
-        pV: 0.3,
-      },
-      {
-        colour: [235, 235, 235],
-        opacity: 0.15,
-        minSize: 100,
-        maxSize: 30000,
-        pH: 0.7,
-        pV: 0.3,
-      },
-      {
-        colour: [235, 235, 235],
-        opacity: 0.15,
-        minSize: 100,
-        maxSize: 10000,
-        pH: 0.7,
-        pV: 0.3,
-      },
-      {
-        colour: [240, 211, 201],
-        opacity: 0.3,
-        minSize: 100,
-        maxSize: 10000,
-        pH: 0.6,
-        pV: 0.2,
-      },
-      {
-        colour: [240, 211, 201],
-        opacity: 0.15,
-        minSize: 100,
-        maxSize: 10000,
-        pH: 0.6,
-        pV: 0.2,
-      },
-      {
-        colour: [173, 216, 230],
-        opacity: 0.1,
-        minSize: 100,
-        maxSize: 10000,
-        pH: 0.6,
-        pV: 0.2,
-      },
-      {
-        colour: [173, 216, 230],
-        opacity: 0.1,
-        minSize: 100,
-        maxSize: 10000,
-        pH: 0.6,
-        pV: 0.2,
-      }
-    ],
-  },
+  sunset: sunsetNone,
+  clouds: cloudsMild
 };
 
 let presetCloudy = {
@@ -588,66 +414,9 @@ let presetCloudy = {
     colour: [115, 160, 200],
     opacity: 1,
   },
-  stars: {
-    include: true,
-    density: 0.01,
-  },
+  stars: starsDense,
   sun: false,
   moon: false,
-  sunset: false,
-  clouds: {
-    include: true,
-    quantity: 30,
-    // Cloud layers are created in order
-    layers: [
-      {
-        colour: [183, 183, 178],
-        opacity: 0.8,
-        minSize: 100,
-        maxSize: 50000,
-        pH: 0.8, // Probability of horizontal expansion
-        pV: 0.5, // Probability of vertical expansion
-      },
-      {
-        colour: [183, 183, 178],
-        opacity: 0.7,
-        minSize: 100,
-        maxSize: 50000,
-        pH: 0.8,
-        pV: 0.5,
-      },
-      {
-        colour: [183, 183, 178],
-        opacity: 0.15,
-        minSize: 100,
-        maxSize: 10000,
-        pH: 0.7,
-        pV: 0.3,
-      },
-      {
-        colour: [183, 183, 178],
-        opacity: 0.3,
-        minSize: 100,
-        maxSize: 10000,
-        pH: 0.7,
-        pV: 0.3,
-      },
-      {
-        colour: [183, 183, 178],
-        opacity: 0.3,
-        minSize: 100,
-        maxSize: 10000,
-        pH: 0.7,
-        pV: 0.3,
-      },
-      {
-        colour: [183, 183, 178],
-        opacity: 0.3,
-        minSize: 100,
-        maxSize: 10000,
-        pH: 0.7,
-        pV: 0.3,
-      },
-    ],
-  },
+  sunset: sunsetNone,
+  clouds: cloudsCloudy
 };
