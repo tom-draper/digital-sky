8fc8dcb0887ecf785f5ab449ec295ed91404b06a
Tom Draper <tomjdraper1@gmail.com>
2022-06-02 21:49:39 +0100

    ui

--- src/config.js
+++ src/config.js
@@ -97,7 +97,7 @@ var config = {
         }
     }
 };
-function init() {
+function fillSkyDefaults() {
     document.getElementById('skyWidth').value = config.sky.properties.width;
     document.getElementById('skyHeight').value = config.sky.properties.height;
     document.getElementById('skyPixelSize').value = config.sky.properties.pixelSize;
@@ -106,6 +106,25 @@ function init() {
     document.getElementById('skyBlue').value = config.sky.properties.colour[2];
     document.getElementById('skyOpacity').value = config.sky.properties.opacity;
     document.getElementById('skyMutationSpeed').value = config.sky.properties.mutationSpeed;
+}
+function fillStarsDefault() {
+    document.getElementById('starsInclude').value = config.stars.include;
+    document.getElementById('starsOpacity').value = config.stars.properties.opacity;
+    document.getElementById('starsDensity').value = config.stars.properties.density;
+}
+function fillMoonDefault() {
+    document.getElementById('moonInclude').value = config.moon.include;
+    document.getElementById('moonRed').value = config.moon.properties.colour[0];
+    document.getElementById('moonGreen').value = config.moon.properties.colour[1];
+    document.getElementById('moonBlue').value = config.moon.properties.colour[2];
+    document.getElementById('moonRadius').value = config.moon.properties.radius;
+    document.getElementById('moonHalfMoon').value = config.moon.properties.halfMoon;
+    document.getElementById('moonNoise').value = config.moon.properties.noise;
+}
+function init() {
+    fillSkyDefaults();
+    fillStarsDefault();
+    fillMoonDefault();
 }
 function run() {
     runSkyGeneration();
--- src/config.ts
+++ src/config.ts
@@ -99,7 +99,7 @@ let config: Config = {
   }
 }
 
-function init() {
+function fillSkyDefaults() {
   document.getElementById('skyWidth').value = config.sky.properties.width;
   document.getElementById('skyHeight').value = config.sky.properties.height;
   document.getElementById('skyPixelSize').value = config.sky.properties.pixelSize;
@@ -108,6 +108,29 @@ function init() {
   document.getElementById('skyBlue').value = config.sky.properties.colour[2];
   document.getElementById('skyOpacity').value = config.sky.properties.opacity;
   document.getElementById('skyMutationSpeed').value = config.sky.properties.mutationSpeed;
+}
+
+function fillStarsDefault() {
+  document.getElementById('starsInclude').value = config.stars.include;
+  document.getElementById('starsOpacity').value = config.stars.properties.opacity;
+  document.getElementById('starsDensity').value = config.stars.properties.density;
+}
+
+function fillMoonDefault() {
+  document.getElementById('moonInclude').value = config.moon.include;
+  document.getElementById('moonRed').value = config.moon.properties.colour[0];
+  document.getElementById('moonGreen').value = config.moon.properties.colour[1];
+  document.getElementById('moonBlue').value = config.moon.properties.colour[2];
+  document.getElementById('moonRadius').value = config.moon.properties.radius;
+  document.getElementById('moonHalfMoon').value = config.moon.properties.halfMoon;
+  document.getElementById('moonNoise').value = config.moon.properties.noise;
+
+}
+
+function init() {
+  fillSkyDefaults()
+  fillStarsDefault()
+  fillMoonDefault()
 }
 
 function run() {
